3e05dba3d7536fd351ecaa61f59fd358d265d18a
Tsewang Stanzin <tstanzin.in@gmail.com>
2020-06-06 14:37:08 +0530

    Update infinite_thread.cpp
    with profiling

--- infinite_thread.cpp
+++ infinite_thread.cpp
@@ -3,6 +3,7 @@
 #include "infinite_thread.h"
 #include "portablesleep.h"
 
+#include "motionprofile.h"
 
 #include "mainwindow.h"
 #include <slalib.h>
@@ -17,30 +18,32 @@
 
 #include <pid.h>
 #include <wiringPi.h>
-
+#include <softPwm.h>
 
 #define MotorPin1 17
-#define MotorPin2 4
+//#define MotorPin2 4
 #define MotorEnableDC1 18
-
-double kp = 1 , ki = 0.1, kd = 0.001;
+double kp =  1.5, ki =7.8 , kd =0.0;    // 1 0.5      0.8    0.9    /best 0.5 6.8   0  //  0.5 7.8 0
 double input=0 , output=0 , setpoint=0 ;
-
+float earlierspd=0;
+int ramp;
 PID myPID(&input, &output, &setpoint, kp, ki, kd, DIRECT);
-
+// speed
+double feed = 4500; // velocity in counts/second
+long target1 = 0; // destination location at any moment
+bool dir = false;
+float accel = 2000.0; // desired acceleration in counts/s^2
 
 #define R2D 57.29577951308232   /* radians to degrees */
 #define R2MAS 206264.806247096355e3 /* radians to mas */
-
 using namespace std;
-
 QString current_time= QTime::currentTime().toString("hhmmss");
- QString current_date= QDate::currentDate().toString();
- QString filename=current_time+current_date+"pidLogging.txt";
+QString current_date= QDate::currentDate().toString();
+QString filename=current_time+current_date+"pidLogging.txt";
+QFile file(filename);
 
- QFile file(filename);
-
-
+QString filename2=current_time+current_date+"TimeLogging.txt";
+QFile file2(filename2);
 InfiniteCountWorker::InfiniteCountWorker(double Gora, double Godec)
     :m_running(true)
 
@@ -49,8 +52,7 @@ InfiniteCountWorker::InfiniteCountWorker(double Gora, double Godec)
     //current_date= QDate::currentDate().toString();
     //filename=current_time+current_date+"DECLogging";
     //filename2=current_time+current_date+"RALogging";
-
-     timer2.start();
+    timer2.start();
     rr=Gora;
     dd=Godec;
     serial = new QSerialPort(this);
@@ -88,44 +90,28 @@ InfiniteCountWorker::InfiniteCountWorker(double Gora, double Godec)
              //ui->Connection->setText((" Controller Connected"));
                //printf("COnnected");
                //qDebug() << "Found the device.\n";
-
-               //QMessageBox::information("Serial Port Error", "Couldn't open serial port to arduino.");
-
+               //QMessageBox::information("Serial Port Error", "Couldn't open serial port.");
            }
-
           }
            else{ QMessageBox msgBox;
               msgBox.setText("The document has been modified.");
-                       //ui->Connection->setText(("Not Connected"));
-                        //qDebug() << "Couldn't find the correct port\n";
-
-                   }    // == WINDOW SETTINGS ==
-
-
+                                  }
 }
 
 void InfiniteCountWorker::doWork()
 {
-
     while(m_running){
-
-        Coord(rr,dd);
-
+        //Coord(rr,dd);
         PID_loop();
-        sendSerialData("$0R2\r");
-        sendSerialData2("$0R1\r");
-
-
-
+        //sendSerialData("$0R2\r");
+        //sendSerialData2("$0R1\r");
         emit updateInfiniteCount(ams,azd,azm,altd,altm,asec,altsec,signRA,signDEC,chour,cminute,csecond,cdeg,camin,casec, signha, signde, ihmsfha[0],ihmsfha[1],ihmsfha[2],idmsfdec[0],idmsfdec[1],idmsfdec[2],parsed_data,parsed_data2);
-
         qApp->processEvents();
-        PortableSleep::msleep(10);
+        PortableSleep::msleep(1);
      }
-
     emit finished();
-
 }
+
 void InfiniteCountWorker::stopWork()
 {
     m_running = false;
@@ -149,8 +135,7 @@ void InfiniteCountWorker::Coord(double Gora, double Godec)
      //double eq, amprms[21];
      //eq=2000;
      //slaMappa(eq,date, amprms);
-     //printf("\n MAPPPPPPPAaaaaa %.30f", amprms[1]);
-    /*User input conversion to degree*/
+     /*User input conversion to degree*/
     /* A star (phi Peg). */
     rc = (Gora*15)/R2D;                  /*RA (ICRS, radians) , Gorc taken as user input , R2D used for degree conversion to radian*/
     dc = Godec/R2D;                  /*Dec (ICRS, radians) , Godc taken as user input*/
@@ -158,7 +143,6 @@ void InfiniteCountWorker::Coord(double Gora, double Godec)
     pd = -35.40/R2MAS;              /* Dec proper motion (radians/year) */
     px = 0.00705;                   /*parallax (arcsec) */
     rv = -14.1;                     /*radial velocity (km/s) */
-
     /* Earth orientation parameters/EOP data (from IERS Bulletin B). */
     //  Values Added from: https://datacenter.iers.org/data/latestVersion/207_BULLETIN_B207.txt
     //  165.338  280.270 -159.1866    0.070  0.221
@@ -173,14 +157,10 @@ void InfiniteCountWorker::Coord(double Gora, double Godec)
     /* nutation adjustment dX */
     dy = 0.221/R2MAS;
     /* nutation adjustment dY */
-
     /* Local circumstances (IAO hanle). */
     // Hanle Long. 78.95917   or 1.37 radain   Lat.   32.77953 or 0.57184
-
     elong = 78.95917/R2D;
-
     /* east longitude (radians) */
-
     phi = 32.77953/R2D;
     /* latitude (radians) */
     hm = 4500;
@@ -195,12 +175,10 @@ void InfiniteCountWorker::Coord(double Gora, double Godec)
     /* observing wavelength (microns) */
     tlr = 0.0065;
     /* lapse rate (K/m) */
-
     /* Aberration correction */
     //slaGeoc( p, h, &r, &z );
     //slaEvp( date, deqx, dvb, dpb, dvh, dph );
     //slaEpv( date, ph, vh, pb, vb );
-
     /* Compute the ICRS-to-CIRS parameters. */
     slaC2ipa ( date, dx, dy, &ciprms, &eo );
     /* Perform the transformation to CIRS. */
@@ -226,25 +204,13 @@ void InfiniteCountWorker::Coord(double Gora, double Godec)
     //tk, pmb, rh, wl, tlr, &aob1, &zob1, &hob, &dob, &rob );
     slaPolmo ( elong, phi, xp, yp, &elong1, &phi1, &daz );
     aob1 += daz;
-
-
      // Converted ra and dec
      double altvalue=(90.0-zob*R2D);
-     double azvalue=(slaDranrm(aob)*R2D);
-
-
+    double azvalue=(slaDranrm(aob)*R2D);
      //Horizon to equatorial coordinates (double precision)
      //slaDh2e( azvalue, altvalue, phi, &hourangle, &equi_dec);
-
      slaDr2tf( 2,hob, &signha,ihmsfha);
      slaDr2af( 2 ,da, &signde,idmsfdec);
-
-
-
-
-       //string dmg;
-       //dmg=DegreesMinutesSeconds(azvalue) ;
-       //cout<< "\n"<< dmg<<"\n";
        bool neg(false) ;
        if (azvalue < 0.0)
        {
@@ -256,9 +222,6 @@ void InfiniteCountWorker::Coord(double Gora, double Godec)
        frac *= 60.0 ;
        int min = (int)frac ;
        frac = frac - (double)min ;
-       // fix the DDD MM 60 case
-       // TODO: nearbyint isnâ€™t alway available (Visual C++,
-       //       for example)
        double sec = nearbyint(frac * 600000.0) ;
        sec /= 10000.0 ;
        if (sec >= 60.0)
@@ -271,17 +234,9 @@ void InfiniteCountWorker::Coord(double Gora, double Godec)
        {
          deg=-deg;
        }
-
        azd = deg;
        azm = min;
        asec = sec;
-
-
-
-
-      //string dmg2;
-      //dmg2=DegreesMinutesSeconds(altvalue) ;
-     //cout<< "\n"<< dmg2<<"\n";
      bool neg2(false) ;
      if (altvalue < 0.0)
      {
@@ -293,9 +248,6 @@ void InfiniteCountWorker::Coord(double Gora, double Godec)
      frac2 *= 60.0 ;
      int min2 = (int)frac2 ;
      frac2 = frac2 - (double)min2 ;
-     // fix the DDD MM 60 case
-     // TODO: nearbyint isnâ€™t alway available (Visual C++,
-     //       for example)
      double sec2 = nearbyint(frac2 * 600000.0) ;
      sec2 /= 10000.0 ;
      if (sec2 >= 60.0)
@@ -312,17 +264,13 @@ void InfiniteCountWorker::Coord(double Gora, double Godec)
     altd = deg2;
      altm = min2;
      altsec= sec2;
-
-
      char sign1, sign2;
      int idmsf[4];
      int ihmsf[4];
-
      converted_ra= slaDranrm(ra);
      converted_dec=da;
      slaDr2tf( 2,converted_ra, &sign1,ihmsf);
      slaDr2af( 2 ,converted_dec, &sign2,idmsf);
-
      signRA=sign1;
      signDEC=sign2;
      chour=ihmsf[0];
@@ -331,23 +279,10 @@ void InfiniteCountWorker::Coord(double Gora, double Godec)
      cdeg=idmsf[0];
      camin=idmsf[1];
      casec=idmsf[2];
-      /*printf("\nRA from main %f",Gora);
-       printf("\nDEC from main %f",Godec);
-       printf("\nApparent RA %f",(converted_ra*R2D/15));
-       printf("\nApparent DA %f",(converted_dec*R2D));*/
     rr=Gora;
     dd=Godec;
-      // rr=converted_ra*R2D/15;
-       //dd=converted_dec*R2D;
-       //pointingCorrection(slaDranrm(aob)*R2D, 90.0-zob*R2D);
-
     double  az,azd,azdd,el,eld,eldd,pa,pad,padd;
     slaAltaz(hob, da, phi,&az, &azd, &azdd, &el, &eld, &eldd, &pa, &pad, &padd);
-
-
-    //printf("\n azz  %f",azd);
-    //printf("\n elll %f",eld);
-    //printf("\n paral angle %f",pad);
 }
 
 
@@ -374,18 +309,6 @@ QByteArray InfiniteCountWorker::readSerialAzimuthEncoder()
     serial->waitForReadyRead(100);
       //pp=serial->readAll();
      parsed_data= serial->readAll();
-    //input = serial.readAll();
-     /*QFile file(filename);
-           if(file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text))
-           {
-               // We're going to streaming text to the file
-               QTextStream stream(&file);
-
-               stream<< current_time<<"\t"<<parsed_data[4]<< parsed_data[5]<< parsed_data[6]<< parsed_data[7]<< parsed_data[8]<< parsed_data[9]<< parsed_data[10]<< parsed_data[11]<<'\n';
-
-               file.close();
-               //qDebug() << "Writing finished";
-           }*/
 
   return parsed_data;
     //qDebug()<<parsed_data;
@@ -415,119 +338,166 @@ void InfiniteCountWorker::readSerialAzimuthEncoder2()
     //const QByteArray input = serial->readAll();
     parsed_data2= serial->readAll();
 
-
-
-    /*QFile file(filename2);
-
-    //QFile file("/home/tsewang/encoder2-data-log.txt");
-
-          if(file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text))
-          {
-              // We're going to streaming text to the file
-              QTextStream stream(&file);
-
-              stream<< current_time<<"\t"<<parsed_data2[4]<< parsed_data2[5]<< parsed_data2[6]<< parsed_data2[7]<< parsed_data2[8]<< parsed_data2[9]<< parsed_data2[10]<< parsed_data2[11]<<'\n';
-
-              file.close();
-              //qDebug() << "Writing finished"<<parsed_data<<"\n";
-          }
-          */
-
 }
-
 
 void InfiniteCountWorker::PID_loop()
 {
+     //angle=azd;
+    //REV= (angle - 0) * (1500 - 0) / (360 - 0) + 0;
+    myPID.SetMode(AUTOMATIC);
+    myPID.SetSampleTime(1);
+    myPID.SetOutputLimits(-524, 524);
+    int destination=20000;     //we want to go here
+    long a1 = millis();
+    long distance = destination - setpoint; // if positive go +x
+    long finalPoint = setpoint + distance;
+    bool dirPos = true;
+   if (distance < 0) {
+        distance = -distance;
+        dirPos = false;
+      }
+    float xm = feed * feed / accel;
+    float f=feed,tt;
+    float t1, t2;
+    if (distance <= xm) { // triangular
+          t1 = t2 = sqrt(distance / accel);
+          f = sqrt (accel * distance);
+      }
+      else { // trapezoidal
+        t1 = sqrt(xm / accel);
+        t2 = (distance - xm) / feed + t1;
+      }
+      float t = 0, spd = 0;
+      float dt = 1e-3;
+      float covered = setpoint;
+      float maxt = t1 + t2;
+      long start=micros();
+      while (t < maxt)
+      {   t += dt;
+          if (t < t1) spd=t*f/t1; else if (t >= t2) spd=(maxt-t)*f/t1;
+        //t=(micros()-start)/1e6;
+        //if (t < t1) {tt=6.28*t/t1; spd=f*(tt-sin(tt))/6.28;} else if (t >= t2) {tt=6.28*(maxt-t)/t1; spd=f*(tt-sin(tt))/6.28; }
+        //if (t < t1) spd=(1-cos(3.14*t/t1))*f/2;   else if (t >= t2) spd=(1-cos(3.14*(maxt-t)/t1))*f/2;
+        if ( dirPos ) covered += spd * dt;
+        else covered -= spd * dt;
+        qDebug()<<"spd"      <<spd;
 
 
+        if(earlierspd<spd) {  qDebug()<<"Accelerating Phase";  ramp=1;}
 
+        else if(earlierspd == spd)  {  qDebug()<<"Constant Phase";  ramp=2;}
 
+        else if(earlierspd>spd) {  qDebug()<<"Deaccelerating Phase";  ramp=3;}
 
-    //lastTime = timer.elapsed()-SampleTime;
-    QByteArray QencoderValue= parsed_data;
-       // qDebug()<< QencoderValue;
+        earlierspd=spd;
 
+        //For reading encoder value from USB-decoder
+        sendSerialData("$0R2\r");
+        sendSerialData2("$0R1\r");
+        QByteArray QencoderValue= parsed_data;
         QencoderValue.remove(12, 14);
-
         QencoderValue.remove(0, 4);
-  encoderValue= QencoderValue.toInt();
-   qDebug()<< "Feedback enc. value       " <<encoderValue;
-
-  myPID.SetMode(AUTOMATIC);
-  myPID.SetSampleTime(1);   //ms
- //softPwmCreate(MotorEnableDC1,0,100);
-  myPID.SetOutputLimits(-500, 500); // this is the MAX PWM value to move motor, here change in value reflect change in speed of motor.
-
-
-
- angle=azd;
-
- REV= (angle - 0) * (1500 - 0) / (360 - 0) + 0;
-
-
-qDebug()<< "Setpoint enc. value         "<<REV;
-
-
-//Serial.print("this is Set Value ");
-//Serial.println(REV);
-
-
-setpoint = REV;
-input = encoderValue ;
- //qDebug()<<encoderValue;
- //Serial.print("encoderValue ");//z
- //Serial.println(encoderValue);
-//Serial.print(" ");
-//Serial.println(output); // qDebug()<< QencoderValue;
-
-
-  myPID.Compute();
-  pwmOut(output);
-
-
-
+        encoderValue= QencoderValue.toInt();
+        qDebug()<< "Feedback enc. value       " <<encoderValue;
+        input = encoderValue;
+        setpoint = covered;
+        qDebug()<< "Setpoint generated"<<setpoint;
+        myPID.Compute();
+        pwmOut(output );
+        qDebug()<< "PID   PWM output         "<<output;
+        //emit updateInfiniteCount(ams,azd,azm,altd,altm,asec,altsec,signRA,signDEC,chour,cminute,csecond,cdeg,camin,casec, signha, signde, ihmsfha[0],ihmsfha[1],ihmsfha[2],idmsfdec[0],idmsfdec[1],idmsfdec[2],parsed_data,parsed_data2);
+        //PortableSleep::msleep(10000);
         if(file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text))
         {
-            // We're going to streaming text to the file
             QTextStream stream(&file);
-
             stream<<timer2.elapsed()<<"\t"<<input<<"\t"<<output<<"\t"<<setpoint<<'\n';
-
             file.close();
-            //qDebug() << "Writing finished";
-        }
+          }
 
+
+        if(file2.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text))
+        {
+            QTextStream stream(&file2);
+            stream<<timer2.elapsed()<<"\t"<<start<<"\t"<<t<<"\t"<<t1<<"\t"<<t2<<"\t"<<maxt<<"\t"<<dt<<"\t"<<spd<<"\t"<<ramp<<'\n';
+            file2.close();
+          }
+              PortableSleep::msleep(1);
+      }
+      target1 = covered;
+
+
+//Open Loop
+/*
+    if(timer2.elapsed()<=60000)
+    {
+ QByteArray QencoderValue= parsed_data;
+   // qDebug()<< QencoderValue;
+
+    QencoderValue.remove(12, 14);
+
+    QencoderValue.remove(0, 4);
+encoderValue= QencoderValue.toInt();
+pwmOut(-1024);
+if(file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text))
+{
+    // We're going to streaming text to the file
+    QTextStream stream(&file);
+
+    stream<<timer2.elapsed()<<"\t"<<1024<<"\t"<<encoderValue<<'\n';
+
+    file.close();
+    //qDebug() << "Writing finished";
+}
+    }*/
 }
 
 void InfiniteCountWorker:: pwmOut(int out) {
-    if(wiringPiSetupGpio()==-1)
-    {
-    cout<<"Setup wiring pi failed";
-    //return 1;
-    }
-else{
-   //softPwmCreate(MotorEnableDC1,0,100);
-  qDebug()<< "PID   PWM output         "<<out;
-  pinMode(MotorPin1,OUTPUT);
-    pinMode(MotorPin2,OUTPUT);
+   wiringPiSetupGpio();
+   pinMode(MotorPin1,OUTPUT);
+   pinMode(MotorEnableDC1,PWM_OUTPUT);
+   //pwmSetMode(PWM_MODE_MS);    //MS
+   pwmSetClock(3840);
+   //pwmSetRange(1250);
+   //pinMode(MotorEnableDC1,OUTPUT);
+   //softPwmCreate(MotorEnableDC1,0,200);
+   if(out>0)
+   {
+    qDebug()<<"ANTI-CLOCKWISE MOTION";
+   pwmWrite(MotorEnableDC1,out);
+   digitalWrite(MotorPin1,LOW);
+   }
+   else if(out<0)
+   {
+    int out2=abs(out);
+    qDebug()<<"CLOCKWISE MOTION";
+
+   pwmWrite(MotorEnableDC1,out2);
+   digitalWrite(MotorPin1,HIGH);
+   }
+   /*
+    pinMode(MotorPin1,OUTPUT);
+    //pinMode(MotorPin2,OUTPUT);
     pinMode(MotorEnableDC1,PWM_OUTPUT);
-
-
-  if (out > 0) {
-      pwmWrite(MotorEnableDC1,out);
-      digitalWrite(MotorPin1,LOW);
-      digitalWrite(MotorPin2,HIGH);
-
+    //pwmSetMode(PWM_MODE_MS);
+    pwmSetClock(1920);
+    pwmSetRange(1250);
+    delay(1);
+    //pinMode(MotorEnableDC1,OUTPUT);
+    //softPwmCreate(MotorEnableDC1,0,200);
+    if (out > 0) {
+    //pwmWrite(MotorEnableDC1,out);
+     pwmWrite(MotorEnableDC1,out);
+     digitalWrite(MotorPin1,LOW);
+      //delay(1000);
+      //digitalWrite(MotorPin2,HIGH);
   }
   else {
       pwmWrite(MotorEnableDC1,out);
-
+      //pwmWrite(MotorEnableDC1,out);
       digitalWrite(MotorPin1,HIGH);
-      digitalWrite(MotorPin2,LOW);
-
-
-  }
-    }
+      // delay(1000);
+      //digitalWrite(MotorPin2,LOW);
+    */
 }
 
 
